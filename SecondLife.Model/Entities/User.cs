f3944cf8703b9d6ab045a4a7e0b992c864dc8fd3
marlon <marlonkh@outlook.fr>
2021-03-01 21:20:54 +0100

    WIP

---
+++ SecondLife.Model/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecondLife.Model.Entities
+{
+    class User
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Mail { get; set; }
+        public int phone { get; set; }
+        public string password {get; set;}
+    }
+}
